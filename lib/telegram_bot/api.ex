8392698ffe9cc18bfbb603fe4e1f87aeb41f585f
Bart Lantz <underhilllabs@gmail.com>
2015-07-07 01:10:12 +0000

    adds variable captions

--- lib/telegram_bot/api.ex
+++ lib/telegram_bot/api.ex
@@ -2,8 +2,8 @@ defmodule TelegramBot.API do
   use HTTPoison.Base
 
   @post_headers %{"Content-type" => "application/x-www-form-urlencoded"}
-  @photos  ["tuvok", "tpol1", "spock1", "spock2", "spock3", "spock4", "spock5", "vulcans1", "tuvok2"]
-  # @post_upload_headers %{"Content-type" => "multipart/form-data"}
+  @photos  ["tuvok", "tpol1", "spock1", "spock2", "spock3", "spock4", "spock5", "tuvok2"]
+  @captions  ["Infinite diversity in infinite combination.", "My mind to your mind... my thoughts to your thoughts...", "In accepting the inevitable, one finds peace.", "Tick tok, Tuvok", "Human math.. I'm crying!", "Insufficient facts always invite danger.", "Once you have eliminated the impossible, whatever remains, however improbable, must be the truth.", "Live long and prosper.", "Not every species is cut out for logic.", "Curious.", "Interesting.", "There's a difference between keeping an open mind and believing something because you want it to be true.", "You're obviously unable to have a physical relationship without developing an emotional attachment.", "It is clear that living among humans has caused my reasoning to become compromised.", "Optimism doesn't alter the laws of physics."]
 
   def process_url(url) do
     "https://api.telegram.org/bot" <> token <> "/" <> url
@@ -25,8 +25,11 @@ defmodule TelegramBot.API do
     post("sendSticker", {:form, [chat_id: chat_id, sticker: sticker]}, @post_headers)
   end
 
-  def send_photo(chat_id, filename \\ "fake", caption \\ "Tick tock, Tuvok") do 
-    filename = "img/"<>sample_pic(@photos)<>".jpg"
+  def send_photo(chat_id, filename \\ "fake", caption \\ nil) do 
+    filename = "img/"<>sample_list(@photos)<>".jpg"
+    unless caption do
+      caption = sample_list(@captions)
+    end
     post("sendPhoto", {:multipart, [{"chat_id", Integer.to_string(chat_id)}, {"caption", caption}, {:file, filename,{"form-data", [{"filename", filename},{"name", "photo"}]}, [{"Content-Type", "image/jpeg"}] }]})
   end
 
@@ -34,7 +37,7 @@ defmodule TelegramBot.API do
     body |> Poison.decode!
   end
 
-  def sample_pic(photos) do
+  def sample_list(photos) do
     Enum.shuffle(photos) |> List.first
   end
 end
